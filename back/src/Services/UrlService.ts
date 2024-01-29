0837a9f9354490bc8c133aecf5368e43700ada06
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2024-01-29 19:27:14 -0300

    feat: add random url

--- back/src/Services/UrlService.ts
+++ back/src/Services/UrlService.ts
@@ -1,14 +1,23 @@
 import * as crypto from "crypto";
-import shortid from "shortid";
 
 export function genShort(longUrl: string) {
   const hash = crypto.createHash("sha256").update(longUrl).digest("hex");
-
-  // Passo 2: Converter a chave para base64
   const base64Key = Buffer.from(hash, "hex").toString("base64");
-
-  // Passo 3: Selecionar os primeiros sete caracteres
   const shortUrl = base64Key.slice(0, 7);
 
-  return shortUrl;
+  const randomChars = generateRandomChars(7 - shortUrl.length);
+  const randomShortUrl = shortUrl + randomChars;
+
+  return randomShortUrl;
+}
+
+function generateRandomChars(length: number): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
 }
