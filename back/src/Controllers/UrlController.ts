3a9ca2a0d939c6407c904050474b4f5453b88486
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2024-01-29 00:25:21 -0300

    feat: add url service

--- back/src/Controllers/UrlController.ts
+++ back/src/Controllers/UrlController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { UrlUseCase } from "../UseCase/UrlUseCase";
+import { genShort } from "../Services/UrlService";
 
 class UrlController {
   constructor(private urlUseCase: UrlUseCase) {}
@@ -7,20 +8,23 @@ class UrlController {
   async getUrl(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
-     const url = await this.urlUseCase.getUrl(id);
+      const url = await this.urlUseCase.getUrl(id);
       return res.json(url);
     } catch (error) {
       console.error("Error in getUrl", error);
-      return res.status(500).json({"Internal Server Error"})
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-}
   async shortenUrl(req: Request, res: Response, next: NextFunction) {
     try {
-        const {id, long } = req.params;      
-        const url = await this.urlUseCase.shortenUrl()
+      const { id, long } = req.params;
+      const short = genShort(long);
+      const url = await this.urlUseCase.shortenUrl(id, long, short);
+      return res.json(url);
     } catch (error) {
-        
+      console.error("Error in shortenUrl", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-}
   }
-export { UrlUseCase }
+}
+export { UrlUseCase };
--- back/src/Services/UrlService.ts
+++ back/src/Services/UrlService.ts
@@ -1,1 +1,14 @@
-export function genShort(longUrl: string) {}
+import * as crypto from "crypto";
+import shortid from "shortid";
+
+export function genShort(longUrl: string) {
+  const hash = crypto.createHash("sha256").update(longUrl).digest("hex");
+
+  // Passo 2: Converter a chave para base64
+  const base64Key = Buffer.from(hash, "hex").toString("base64");
+
+  // Passo 3: Selecionar os primeiros sete caracteres
+  const shortUrl = base64Key.slice(0, 7);
+
+  return shortUrl;
+}
